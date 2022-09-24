0616ade574865b9b73a8f28d6a4db088a2b52bad
JustSemen <semyn03@mail.ru>
2022-09-24 16:46:28 +0300

    remove required for PostForm

--- src/comonents/Profile/PostForm/PostForm.jsx
+++ src/comonents/Profile/PostForm/PostForm.jsx
@@ -3,64 +3,10 @@ import MyButton from "../../UI/MyButton/MyButton";
 import MyInput from '../../UI/MyInput/MyInput';
 import style from './PostForm.module.css';
 import { Form, Field } from 'react-final-form'
-import { composeValidators, maxLengthCreator, required } from '../../../utils/validators';
+import { maxLengthCreator } from '../../../utils/validators';
 import MyTextarea from '../../UI/MyTextarea/MyTextarea';
 
 const PostForm = ({ onCreatePost, profilePage, ...props }) => {
-    /* const [title, setUsername] = useState('')
-    const [body, setMessage] = useState('')
-    const keyMessages = {
-        'привет, вероника': `Привет, ${title}!`,
-        'который час?': getTime(),
-        'сколько будет': veronikaCalc(body) /* Калькулятор однозначных чисел 
-    } */
-
-    /* function veronikaCalc(string) {
-        let arrayOfNumbers = []
-        if (string.search(/\d/) != -1) {
-            for (let i = 0; i < string.length; i++) {
-                for (let j = 0; j < 10; j++) {
-                    if (string[i] == j) arrayOfNumbers.push(string[i])
-                }
-            }
-        }
-
-        let result = 0
-        arrayOfNumbers.forEach((number) => {
-            result += Number(number)
-        })
-
-        return `Результат сложения ваших однозначных чисел равен ${result}`
-    }
-
-    function getTime() {
-        let date = new Date
-        return (`Сейчас ${date.getHours()}`)
-    }
-
-    function createPost(e) {
-        dispatch()
-    }
-
-    useEffect(() => {
-        for (let keyMessage in keyMessages) {
-            if (body.toLocaleLowerCase().includes(keyMessage)) {
-                const botMessage = {
-                    id: Date.now(),
-                    avatar: veronika,
-                    title: 'Вероника',
-                    body: keyMessages[keyMessage]
-                }
-                if (body.includes('сколько будет')) {
-                    veronikaCalc(body)
-                }
-                setPost([...posts, botMessage])
-            }
-        }
-
-        setUsername('')
-        setMessage('')
-    }, [posts]) */
 
     const onSubmit = (formData) => {
         onCreatePost(formData.newPostText, formData.newPostTitleText)
@@ -68,19 +14,9 @@ const PostForm = ({ onCreatePost, profilePage, ...props }) => {
         formData.newPostTitleText = ''
     }
 
-    const validate = (formData) => {
-        const errors = {}
-
-        if (!formData.newPostText)
-
-
-            return errors
-    }
-
     return (
         <Form
             onSubmit={onSubmit}
-            validate={validate}
             render={({ handleSubmit, invalid }) => (
                 <form className={style.newPost} onSubmit={handleSubmit}>
                     <Field
@@ -93,7 +29,7 @@ const PostForm = ({ onCreatePost, profilePage, ...props }) => {
                         )}
                     />
                     <Field
-                        validate={composeValidators(required, maxLengthCreator(50))}
+                        validate={maxLengthCreator(50)}
                         name='newPostText'
                         component={MyTextarea}
 
