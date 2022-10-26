8ebcdaf93987f7d11ef1ac38ff2e82c311f387f2
JustSemen <semyn03@mail.ru>
2022-10-26 14:19:27 +0600

    add beautiful login

--- src/comonents/Login/Login.tsx
+++ src/comonents/Login/Login.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import MyInput from '../UI/MyInput/MyInput'
+import style from './Login.module.css'
 import errorStyle from '../UI/MyInput/MyInput.module.css'
-import buttonStyle from '../UI/MyButton/MyButton.module.css'
-import MyButton from '../UI/MyButton/MyButton'
 import { required } from '../../utils/validators'
 import { Form, Field } from 'react-final-form'
 import { connect } from 'react-redux';
@@ -23,12 +21,13 @@ const Login: React.FC<LoginPropsType> = ({ login, isAuth, isCaptcha }) => {
 
     return (
         <div>
-            <h1>Login</h1>
-            <LoginForm login={login} isCaptcha={isCaptcha} />
-            <p style={{ 'fontSize': '17px' }}>Авторизация реализована с использованием серверного API social-network-samuraiJS.</p>
+            <h1>Log In</h1>
+            <div className={style.wrapper}>
+                <LoginForm login={login} isCaptcha={isCaptcha} />
+            </div>
+            {/* <p style={{ 'fontSize': '17px' }}>Авторизация реализована с использованием серверного API social-network-samuraiJS.</p>
             <p style={{ 'fontSize': '17px' }}>email: semyn03@mail.ru</p>
-            <p style={{ 'fontSize': '17px' }}>password: QWERTY123</p>
-            <a className={buttonStyle.button} href='https://social-network.samuraijs.com/signUp'>Зарегистрироваться</a>
+            <p style={{ 'fontSize': '17px' }}>password: QWERTY123</p> */}
         </div>
     );
 };
@@ -54,17 +53,18 @@ const LoginForm = ({ login, isCaptcha }: LoginFormType) => {
         <Form
             onSubmit={onSubmit}
             render={({ handleSubmit, submitError }) => (
-                <form onSubmit={handleSubmit}>
+                <form className={style.signIn} onSubmit={handleSubmit}>
                     <Field
                         name='email'
-                        component={MyInput}
+                        component={'input'}
                         validate={required}
+                        type='text'
 
                         placeholder='Email'
                     />
                     <Field
                         name='password'
-                        component={MyInput}
+                        component={'input'}
                         validate={required}
 
                         type='password'
@@ -73,17 +73,13 @@ const LoginForm = ({ login, isCaptcha }: LoginFormType) => {
                     <Field
                         name='rememberMe'
                         type={'checkbox'}
-                        render={({ input, meta }) => (
-                            <div style={{ marginBottom: '5px' }}>
-                                <input {...input} />remember me
-                            </div>
-                        )}
-                    />
+                        component={'input'}
+                    />Запомнить меня
                     <div id='captcha'></div>
                     {isCaptcha ?
                         <Field
                             name='captcha'
-                            component={MyInput}
+                            component={'input'}
 
                             placeholder='Captcha'
                         />
@@ -93,9 +89,10 @@ const LoginForm = ({ login, isCaptcha }: LoginFormType) => {
                         <div className={errorStyle.formSummaryError}>
                             {submitError}
                         </div> : ''}
-                    <div>
-                        <MyButton>Login</MyButton>
+                    <div className={style.btnsWrapper}>
+                        <button type='submit'>▶</button>
                     </div>
+                    <p>Don't you have an account? <a href="https://social-network.samuraijs.com/signUp">зарегистрироваться</a></p>
                 </form>
             )}
         >
